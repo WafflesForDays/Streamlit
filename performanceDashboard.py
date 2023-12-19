ad03d2d2201a9cc66460394ef431421cc5973eec
Ted Pavlov <triplepokemondeluxe@gmail.com>
2023-12-19 10:52:31 -0500

    Changes to be committed: 	modified:   performanceDashboard.py

--- performanceDashboard.py
+++ performanceDashboard.py
@@ -1,14 +1,20 @@
 import streamlit as st
 import time
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns(2, gap = "small")
 st.set_page_config(layout = "wide")
 st.title('Student Dashboard')
-col1.header('Volunteer hours completed:')
-col1.text('40/40')
-progress_bar = col2.progress(0, use_column_width = True)
+with col1:
+    st.header('Volunteer hours completed:')
+    st.text('40/40')
+with col2:
+    progress_bar = st.progress(0, use_column_width = True)
 for i in range(100):
     progress_bar.progress(i + 1)
     time.sleep(0.01)
-col1.header('Top 6 courses:')
+expanderCourses = st.expander(label = 'Show Courses')
+with expanderCourses:
+    clicked = col1.header('Top 6 courses:')
+
+
 
