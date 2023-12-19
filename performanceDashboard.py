d3880de94153eb94419b999e6a5825291e6f1a63
Ted Pavlov <triplepokemondeluxe@gmail.com>
2023-12-19 10:33:38 -0500

    Changes to be committed: 	modified:   performanceDashboard.py

--- performanceDashboard.py
+++ performanceDashboard.py
@@ -1,8 +1,10 @@
 import streamlit as st
 
-progress_bar = st.progress(0)
 st.title('Student Dashboard')
-st.header('Volunteer hours completed')
+st.header('Volunteer hours completed:')
+st.text('40/40')
+progress_bar = st.progress(0)
 for i in range(40):
     progress_bar.progress(i + 1)
+    time.sleep(0.1)
 st.header('Top 6 courses:')
