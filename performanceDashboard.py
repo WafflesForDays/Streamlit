8caf59984b2c56d10bc01808caad525b9176c7df
Ted Pavlov <triplepokemondeluxe@gmail.com>
2023-12-19 10:23:40 -0500

    Changes to be committed: 	modified:   performanceDashboard.py

--- performanceDashboard.py
+++ performanceDashboard.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title('Student Dashboard')
 courses = st.number_input('How many courses do you have this semester?')
 for i in range(courses):
-    st.slider(f'What is your average in your period {i} class')
+    st.slider(f'What is your average in your period {i} class', min_value = 0, max_value = 100)
 
